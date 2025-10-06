1c7c348f03b3f4bdcb9d4bfdc68851ac33dcd8d8
MaxStrikanov <maxvell.88@mail.ru>
2025-10-06 17:55:16 +0300

    Карта

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ import changeColor from './public/components/runBtn';
 import magnifier from './public/components/magnifier';
 import accordion from './public/components/accordion';
 
+import './public/components/step';
 
 changeColor()
 magnifier()
+
 
 accordion()
